791b28743e746503c748bb46526ae2ad4f9d9edf
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 06:19:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested').querySelector(div.target);
+  return document.getElementById('nested').querySelector('div.target');
 }
