851f0cf6048fe844826b3308e7a09839e511ef95
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 06:35:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll('.ranked-list');
+  for (var i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = (i + n).toString();
+  }
+}
+
+function deepestChild() {
+  var list = document.getElementById('grand-node').querySelectorAll('div');
+  var deepest;
+  for (var i = 0; i < list.length-1; i++) {
+    deepest = list[i].querySelector('div');
+  }
+  return deepest;
+}
